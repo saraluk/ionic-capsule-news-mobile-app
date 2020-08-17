a66c8368493e09cb592bb4028326840ff980ab18
saraluk <saralukkai@gmail.com>
2020-08-17 18:17:29 -0400

    changed theme color

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class NewsService {
   topHeadLineNewsUrl =
-    "https://newsapi.org/v2/top-headlines?apiKey=9d82d24487ea42bdac74d32ef696e7e7&sources=bbc-news&sortBy=popularity";
+    'https://newsapi.org/v2/top-headlines?apiKey=9d82d24487ea42bdac74d32ef696e7e7&sources=bbc-news&sortBy=popularity';
 
   businessNewsUrl =
-    "https://newsapi.org/v2/top-headlines?apiKey=9d82d24487ea42bdac74d32ef696e7e7&category=business&country=us&sortBy=popularity&pageSize=5";
+    'https://newsapi.org/v2/top-headlines?apiKey=9d82d24487ea42bdac74d32ef696e7e7&category=business&country=us&sortBy=popularity&pageSize=5';
 
   technologyNewsUrl =
-    "https://newsapi.org/v2/top-headlines?apiKey=9d82d24487ea42bdac74d32ef696e7e7&category=technology&country=us&sortBy=popularity&pageSize=5";
+    'https://newsapi.org/v2/top-headlines?apiKey=9d82d24487ea42bdac74d32ef696e7e7&category=technology&country=us&sortBy=popularity&pageSize=5';
   constructor(private http: HttpClient) {}
 
   getTopHeadLineNews() {
